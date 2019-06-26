d6395e5871aff35adaf6d75bc27c6d9cf165fc14
Kellen Baker <mkellenbaker@gmail.com>
2019-06-26 10:53:52 -0500

    add security clearance levels to /secrets

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,12 +5,32 @@ const { rejectUnauthenticated } = require("../modules/authentication-middleware"
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    if (req.user.clearance_level <= 5) {
+    pool.query(`SELECT * FROM "secret"
+                WHERE "secrecy_level" < 5;`)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    } else if (req.user.clearance_level <= 10) {
+    pool.query(`SELECT * FROM "secret"
+                WHERE "secrecy_level" < 10;`)
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        });
+    } else if (req.user.clearance_level > 10) {
+          pool.query(`SELECT * FROM "secret"`)
+        .then(results => res.send(results.rows))
+        .catch(error => {
+            console.log('Error making SELECT for secrets:', error);
+            res.sendStatus(500);
+        })
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
