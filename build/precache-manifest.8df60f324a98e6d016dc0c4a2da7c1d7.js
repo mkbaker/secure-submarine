89e55d96d05d1fe20e2828c46aff0cafe0038d5f
Kellen Baker <mkellenbaker@gmail.com>
2019-06-26 13:19:10 -0500

    create build

---
+++ build/precache-manifest.8df60f324a98e6d016dc0c4a2da7c1d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6643cacff1592c02fcf3d5f5fca44518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca98def827b40be09a3",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "a9791c0e3a1dc88ee73c",
+    "url": "/static/js/2.334b9d6a.chunk.js"
+  },
+  {
+    "revision": "3ca98def827b40be09a3",
+    "url": "/static/js/main.44438392.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
